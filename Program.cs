5bf2d6f5d7a278b8353695d10dc6bbf5690a765a
LMMoonlight <60115536+LMMoonlight@users.noreply.github.com>
2020-01-20 21:40:05 -0500

    Add files via upload
    Ejercicio en clase con cordenadas

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+      static void Main()
+        {
+            Console.WriteLine("ingrese X1");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese Y1");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese X2");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese Y2");
+            double y2 = double.Parse(Console.ReadLine());
+
+            double pendiente = (y2 - y1) / (x2 - x1);
+            double intercepto = y1 - (pendiente * x1);
+            double distancia = Math.Sqrt(((y2 - y1) * (y2 - y1)) + ((x2 - x1) * (x2 - x1)));
+
+            Console.WriteLine("La pendiente es:" + pendiente + "Y el intercepto es" + intercepto);
+            Console.WriteLine("La distancia es:" + distancia);
+        }
+    }
+}
