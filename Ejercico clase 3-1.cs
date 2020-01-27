c846f16f79d367e6258fbbfe59188f39f169b7fb
LMMoonlight <60115536+LMMoonlight@users.noreply.github.com>
2020-01-27 15:37:54 -0500

    Add files via upload

---
+++ Ejercico clase 3-1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese su peso: ");
+            double peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su altura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double IMC = peso * (Math.Pow(altura, 2));
+
+            if (IMC <= 18.49)
+            {
+                Console.WriteLine("Usted esta bajo de peso");
+            }
+            else if (18.5 <= IMC && IMC < 25.0)
+            {
+                Console.WriteLine("Su peso es normal");
+            }
+            else if (25.0 <= IMC && IMC < 30.0)
+            {
+                Console.WriteLine("Usted tiene sobrepeso");
+            }
+            else
+            {
+                Console.WriteLine("Usted esta obeso");
+            }
+
+        }
+    }
+}
---
+++ Ejercico clase 3-2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_clase_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese a");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese b");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese c");
+            double c = double.Parse(Console.ReadLine());
+
+            double d = Math.Pow(b, 2) - (4.0 * a * c);
+
+            if (d == 0.0)
+            {
+                double d1 = -b / (2 * a);
+                Console.WriteLine("La solución existe y es única");
+                Console.WriteLine("La solucion es: " + d1);
+            }
+            else if (d > 0)
+            {
+                double d2 = (-b + Math.Sqrt(d)) / (2.0*a);
+                double d3 = (-b - Math.Sqrt(d)) / (2.0 * a);
+                Console.WriteLine("Existen dos soluciones correctas");
+                Console.WriteLine("La solución 1 es: " + d2 + "y la solucion 2 es: " + d3);
+
+            }
+            else
+            {
+                Console.WriteLine("No es posible calcular.");
+            }
+        }
+    }
+}
