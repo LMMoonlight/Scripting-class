54bc0927982e94c9840127c716aadbc55952c2a4
LMMoonlight <60115536+LMMoonlight@users.noreply.github.com>
2020-01-30 18:55:42 -0500

    Update Tarifas.cs

--- Tarifas.cs
+++ Tarifas.cs
@@ -13,15 +13,15 @@ namespace Tarifas
             Console.WriteLine("Ingrese cuantos smmlv gana usted");
             double s = double.Parse(Console.ReadLine());
 
-            if (0 > s)
+            if (0.0 > s)
             {
                 Console.WriteLine("No es posible tener un salario mensual negativo, no sea mamón.");
             }
-            else if (0 < s && s < 2)
+            else if (0.0 < s && s < 2.0)
             {
                 Console.WriteLine("Usted tiene la tarifa A.");
             }
-            else if (2 <= s && s < 4)
+            else if (2.0 <= s && s < 4.0)
             {
                 Console.WriteLine("Usted tiene la tarifa B.");
             }              
