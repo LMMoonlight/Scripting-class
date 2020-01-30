9ce1c5c1215dda70b7bb4b68a6f3148352c7a840
LMMoonlight <60115536+LMMoonlight@users.noreply.github.com>
2020-01-30 18:43:27 -0500

    Add files via upload

---
+++ Tarifas.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarifas
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese cuantos smmlv gana usted");
+            double s = double.Parse(Console.ReadLine());
+
+            if (0 > s)
+            {
+                Console.WriteLine("No es posible tener un salario mensual negativo, no sea mamón.");
+            }
+            else if (0 < s && s < 2)
+            {
+                Console.WriteLine("Usted tiene la tarifa A.");
+            }
+            else if (2 <= s && s < 4)
+            {
+                Console.WriteLine("Usted tiene la tarifa B.");
+            }              
+            else
+            {
+                Console.WriteLine("Usted Tiene la tarifa C.");
+            }
+
+
+        }
+    }
+}
