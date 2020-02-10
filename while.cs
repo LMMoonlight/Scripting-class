0719cbe518c6ba4d55a488e0337776407b2bbd71
LMMoonlight <60115536+LMMoonlight@users.noreply.github.com>
2020-02-10 14:56:14 -0500

    Add files via upload

---
+++ while.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main()
+        {
+            double total = 0;
+            Console.WriteLine("Ingrese el número de datos (n): ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 0, max = 0, min = 200;
+            string mayor = "n", menor = "n";
+
+            while(i < n)
+            {
+                Console.Write("Edad: ");
+                int edad = int.Parse(Console.ReadLine());
+                Console.Write("Nombre: ");
+                string nombre = Console.ReadLine();
+
+                if (edad > max)
+                {
+                    max = edad;
+                    mayor = nombre;
+                }
+                if (edad < min)
+                {
+                    min = edad;
+                    menor = nombre;
+                }
+                total += edad;
+                i++;
+            }
+
+            double promedio = total / n;
+            Console.WriteLine("Promedio =" + promedio);
+            Console.WriteLine("Mayor = " + max + " y su nombre es: " + mayor);
+            Console.WriteLine("Menor =" + min + " y su nombre es: " + menor);
+        }
+    }
+}
